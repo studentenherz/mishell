65b2002cb06187e285e7a1a472b54361143a0eab
Michel Romero <studentenherz@gmail.com>
2025-11-01 19:42:19 -0300

    feat: command not found

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    println!("{}: command not found", command.trim());
 }
