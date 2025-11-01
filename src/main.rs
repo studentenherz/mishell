0495e491124dab7083535002701dee6bceaa887b
Michel Romero <studentenherz@gmail.com>
2025-11-01 19:48:31 -0300

    feat: basic REPL (add the loop)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 use std::io::{self, Write};
 
-fn main() {
+fn print_prompt() {
     print!("$ ");
     io::stdout().flush().unwrap();
+}
+
+fn read_command() -> String {
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-    println!("{}: command not found", command.trim());
+
+    command.trim().to_string()
+}
+
+fn eval(command: &str) {
+    println!("{}: command not found", command);
+}
+
+fn main() {
+    loop {
+        print_prompt();
+        let command = read_command();
+        eval(&command);
+    }
 }
