1a455f6b5f24e472af7cf7b72423d3a96a86360d
Michel Romero <studentenherz@gmail.com>
2025-11-01 23:30:51 -0300

    fix: check if file is executable in locate

--- src/locate.rs
+++ src/locate.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
 
 use crate::builtins::get_builtin;
@@ -19,8 +20,12 @@ pub fn locate(cmd: &str) -> LocatedCommand {
     if let Some(path) = env::var_os("PATH") {
         for dir in env::split_paths(&path) {
             let executable_path = dir.join(cmd);
-            if matches!(fs::exists(&executable_path), Ok(true)) {
-                return LocatedCommand::Executable(executable_path);
+            if matches!(executable_path.try_exists(), Ok(true)) {
+                if let Ok(metadata) = fs::metadata(&executable_path) {
+                    if metadata.is_file() && (metadata.permissions().mode() & 0o111) != 0 {
+                        return LocatedCommand::Executable(executable_path);
+                    }
+                }
             }
         }
     }
