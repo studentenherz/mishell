51e340937b2576738df9fbd961960b8e064f4d4c
Michel Romero <studentenherz@gmail.com>
2025-11-01 22:35:12 -0300

    feat: implement `type` builtin

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -2,14 +2,17 @@ use crate::commands::Command;
 
 mod echo;
 mod exit;
+mod type_;
 
 use echo::Echo;
 use exit::Exit;
+use type_::Type;
 
 pub fn get_builtin(cmd: &str) -> Option<Box<dyn Command>> {
     match cmd {
         "exit" => Some(Box::new(Exit::new())),
         "echo" => Some(Box::new(Echo::new())),
+        "type" => Some(Box::new(Type::new())),
         _ => None,
     }
 }
---
+++ src/builtins/type_.rs
@@ -0,0 +1,28 @@
+use crate::args::Args;
+use crate::commands::*;
+use crate::locate::{locate, LocatedCommand};
+
+pub struct Type;
+
+impl Type {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Command for Type {
+    fn eval(&self, args: Args) -> CommandReturnType {
+        if let Some(cmd) = args.args.iter().nth(1) {
+            match locate(cmd) {
+                LocatedCommand::Builtin(_) => {
+                    println!("{} is a shell builtin", cmd)
+                }
+                LocatedCommand::Unrecognized => {
+                    println!("{}: not found", cmd);
+                }
+            }
+        }
+
+        CommandReturnType {}
+    }
+}
