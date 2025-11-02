dd4bec7c91184b03de8e55e318513471ad8995ea
Michel Romero <studentenherz@gmail.com>
2025-11-02 01:41:44 -0300

    feat: reaplace ~ with HOME env variable

--- src/builtins/cd.rs
+++ src/builtins/cd.rs
@@ -14,16 +14,19 @@ impl Cd {
 
 impl Command for Cd {
     fn eval(&self, args: Args) -> CommandReturnType {
-        if let Some(arg1) = args.args.iter().nth(1) {
-            let target_dir = Path::new(arg1);
-            if matches!(target_dir.try_exists(), Ok(true)) && target_dir.is_dir() {
-                if env::set_current_dir(target_dir).is_ok() {
-                    return CommandReturnType {};
-                }
-            }
+        let arg1 = args.args.iter().nth(1).map_or("~", |x| x.as_str());
+        let home_dir = env::home_dir().map_or("~".to_string(), |x| x.display().to_string());
 
-            println!("cd: {}: No such file or directory", arg1);
+        let target_dir = &arg1.replace("~", &home_dir);
+        let target_dir_path = Path::new(target_dir);
+
+        if matches!(target_dir_path.try_exists(), Ok(true)) && target_dir_path.is_dir() {
+            if env::set_current_dir(target_dir_path).is_ok() {
+                return CommandReturnType {};
+            }
         }
+
+        println!("cd: {}: No such file or directory", arg1);
 
         CommandReturnType {}
     }
